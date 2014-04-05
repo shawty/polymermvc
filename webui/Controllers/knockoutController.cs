c3d4bf1906b47aacedb6fb520beec2028aa6dbb0
shawty <shawty_ds@yahoo.com>
2014-04-05 18:46:54 +0100

    Some experiments, updates and a synch between desktop and laptop

---
+++ webui/Controllers/knockoutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using webui.Models;
+
+namespace webui.Controllers
+{
+  public class knockoutController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult GetData()
+    {
+      List<KoDataViewModel> dataToReturn = new List<KoDataViewModel>
+      {
+        new KoDataViewModel{ Id = 1, Name = "Peter", NickName = "Shawty"},
+        new KoDataViewModel{ Id = 2, Name = "Samantha", NickName = "Sam"},
+        new KoDataViewModel{ Id = 3, Name = "Rufus", NickName = "Buggerlugs"}
+      };
+
+      return Json(dataToReturn, JsonRequestBehavior.AllowGet);
+    }
+
+  }
+}
---
+++ webui/Models/KoDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webui.Models
+{
+  public class KoDataViewModel
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string NickName { get; set; }
+  }
+}
